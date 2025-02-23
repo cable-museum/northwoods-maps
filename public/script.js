e79bb2abaed1acfc74e62c8203da96fb92bdd800
Ali Berlin Johnson <allberlinjohnson@gmail.com>
2025-02-23 14:51:38 -0600

    clickies first pass

--- public/script.js
+++ public/script.js
@@ -15,6 +15,99 @@ function toggleSVG(treeName) {
         img.style.display = "none";
     }
 }
+/*------ MAP BUTTONS ------*/
+
+// When a user clicks... 
+// - Get the data-map values
+// - Check for classes
+// IF card "is-selected"... 
+// - remove "is-selected" from card
+// - remove "is-visisble" from data-map
+// - IF another map "is-visisble"...
+// - - add "is-addable" too all other cards in same parent card-container
+// ELSE IF card is "is-addable"
+// - add "is-selected" to card
+// - add "is-visisble" to data-map
+// - remove "is-addable" from all cards
+// ELSE --- THIS IS WRONG!
+// - add "is-selected" to card
+// - add "is-visible" to data-map
+// - add "is-addable" to all other cards in same parent card-container
+
+function hideMap(map) {
+  document.getElementById(map).classList.remove("is-visible");
+}
+
+function showMap(map) {
+  document.getElementById(map).classList.add("is-visible");
+}
+
+document.querySelectorAll(".card").forEach(card => {
+  card.addEventListener("click", function(event) {
+    const clickedCard = event.currentTarget; // Parent .card div clicked on
+    const allCardsInSection = [...card.parentElement.children]; // All cards in same card-container
+    const allCards =  document.querySelectorAll(".card.is-selected");
+    const map = card.getAttribute("data-map");
+    
+    if (clickedCard.classList.contains("is-selected")) {
+      clickedCard.classList.remove("is-selected");
+      hideMap(map);
+
+      // If there is another selected card, add "is-addable" to all others
+      if (allCardsInSection.find(card => card.classList.contains("is-selected"))) {
+        allCardsInSection.forEach(card => {
+          if (!card.classList.contains("is-selected")) {
+            card.classList.add("is-addable");
+          }
+        });
+      }
+    }
+    else if (card.classList.contains("is-addable")) {
+      clickedCard.classList.add("is-selected");
+      clickedCard.classList.remove("is-addable");
+      showMap(map);
+
+      // Remove "is-addable" to all cards in section
+      allCardsInSection.forEach(card => {
+        if (card.classList.contains("is-addable")) {
+          card.classList.remove("is-addable");
+        }
+      });
+    }
+    else {
+      // Remove any cards with class "is-selected" in both sections
+      allCards.forEach(card => {
+        card.classList.remove("is-selected")
+      });
+      
+      clickedCard.classList.add("is-selected");
+      // Hide all maps
+      showMap(map);
+      
+      // Add "is-addable" to other cards sections
+      allCardsInSection.forEach(card => {
+        if (card !== clickedCard) {
+          card.classList.add("is-addable");
+        }
+      });
+
+    }
+  });
+});
+
+
+// function buttonClick(event) {
+//   event.preventDefault();
+//   console.log('#### event', event.target);
+//     // const button = document.querySelector(`[data-map="${name}"]`);
+//     // const map = document.getElementById(name);
+//     // console.log(button);
+//     // console.log(map);
+// }
+
+// document
+//     .querySelector('.card')
+//     .addEventListener("click", (event) => buttonClick(event));
 
 /*------ MAP ANIMATION ------*/
 
