a16bf5bb39bca00f9a37448a6529285961f85f7c
Geoffry Sumter <geoffry@descript.com>
2025-02-23 17:01:37 -0600

    Fix formatting and animations

--- public/script.js
+++ public/script.js
@@ -15,98 +15,80 @@ function toggleSVG(treeName) {
         img.style.display = "none";
     }
 }
+
 /*------ MAP BUTTONS ------*/
 
-// When a user clicks... 
-// - Get the data-map values
-// - Check for classes
-// IF card "is-selected"... 
-// - remove "is-selected" from card
-// - remove "is-visisble" from data-map
-// - IF another map "is-visisble"...
-// - - add "is-addable" too all other cards in same parent card-container
-// ELSE IF card is "is-addable"
-// - add "is-selected" to card
-// - add "is-visisble" to data-map
-// - remove "is-addable" from all cards
-// ELSE --- THIS IS WRONG!
-// - add "is-selected" to card
-// - add "is-visible" to data-map
-// - add "is-addable" to all other cards in same parent card-container
-
 function hideMap(map) {
-  document.getElementById(map).classList.remove("is-visible");
+    document.getElementById(map).classList.remove("is-visible");
 }
 
 function hideAllMaps() {
-  document.querySelectorAll(".overlay").forEach(map => {
-    map.classList.remove("is-visible");
-  });
+    document.querySelectorAll(".overlay").forEach((map) => {
+        map.classList.remove("is-visible");
+    });
 }
 
 function showMap(map) {
-  document.getElementById(map).classList.add("is-visible");
+    document.getElementById(map).classList.add("is-visible");
 }
 
-document.querySelectorAll(".card").forEach(card => {
-  card.addEventListener("click", function(event) {
-    const clickedCard = event.currentTarget; // Parent .card div clicked on
-    const allCardsInSection = [...card.parentElement.children]; // All cards in same card-container
-    const allCards =  document.querySelectorAll(".card.is-selected");
-    const map = card.getAttribute("data-map");
-    
-    if (clickedCard.classList.contains("is-selected")) {
-      clickedCard.classList.remove("is-selected");
-      hideMap(map);
+document.querySelectorAll(".card").forEach((card) => {
+    card.addEventListener("click", function (event) {
+        const clickedCard = event.currentTarget; // Parent .card div clicked on
+        const allCardsInSection = [...card.parentElement.children]; // All cards in same card-container
+        const allCards = document.querySelectorAll(".card.is-selected");
+        const map = card.getAttribute("data-map");
 
-      // If there is another selected card, add "is-addable" to all others
-      if (allCardsInSection.find(card => card.classList.contains("is-selected"))) {
-        allCardsInSection.forEach(card => {
-          if (!card.classList.contains("is-selected")) {
-            card.classList.add("is-addable");
-          }
-        });
-      }
-      else {
-        allCardsInSection.forEach(card => {
-          card.classList.remove("is-addable");
-      });
-      }
+        if (clickedCard.classList.contains("is-selected")) {
+            clickedCard.classList.remove("is-selected");
+            hideMap(map);
 
-    }
-    else if (card.classList.contains("is-addable")) {
-      clickedCard.classList.add("is-selected");
-      clickedCard.classList.remove("is-addable");
-      showMap(map);
+            // If there is another selected card, add "is-addable" to all others
+            if (
+                allCardsInSection.find((card) =>
+                    card.classList.contains("is-selected")
+                )
+            ) {
+                allCardsInSection.forEach((card) => {
+                    if (!card.classList.contains("is-selected")) {
+                        card.classList.add("is-addable");
+                    }
+                });
+            } else {
+                allCardsInSection.forEach((card) => {
+                    card.classList.remove("is-addable");
+                });
+            }
+        } else if (card.classList.contains("is-addable")) {
+            clickedCard.classList.add("is-selected");
+            clickedCard.classList.remove("is-addable");
+            showMap(map);
 
-      // Remove "is-addable" to all cards in section
-      allCardsInSection.forEach(card => {
-        if (card.classList.contains("is-addable")) {
-          card.classList.remove("is-addable");
+            // Remove "is-addable" to all cards in section
+            allCardsInSection.forEach((card) => {
+                if (card.classList.contains("is-addable")) {
+                    card.classList.remove("is-addable");
+                }
+            });
+        } else {
+            // Hide all maps Remove any cards with class "is-selected" in both sections
+            hideAllMaps();
+            allCards.forEach((card) => {
+                card.classList.remove("is-selected");
+            });
+
+            clickedCard.classList.add("is-selected");
+            showMap(map);
+
+            // Add "is-addable" to other cards sections
+            allCardsInSection.forEach((card) => {
+                if (card !== clickedCard) {
+                    card.classList.add("is-addable");
+                }
+            });
         }
-      });
-    }
-    else {
-      // Hide all maps Remove any cards with class "is-selected" in both sections
-      hideAllMaps();
-      allCards.forEach(card => {
-        card.classList.remove("is-selected")
-      });
-      
-      clickedCard.classList.add("is-selected");
-      showMap(map);
-      
-      // Add "is-addable" to other cards sections
-      allCardsInSection.forEach(card => {
-        if (card !== clickedCard) {
-          card.classList.add("is-addable");
-        }
-      });
-
-    }
-  });
+    });
 });
-
 
 // function buttonClick(event) {
 //   event.preventDefault();
@@ -123,7 +105,6 @@ document.querySelectorAll(".card").forEach(card => {
 
 /*------ MAP ANIMATION ------*/
 
-const containerIds = ["animated-redoak", "animated-maple"];
 const stopAnimations = new Map();
 
 function createAnimation(containerId) {
@@ -173,23 +154,23 @@ function createAnimation(containerId) {
     return () => cancelAnimationFrame(animationId); // Return stop function
 }
 
-function stopAnimation(containerId) {
-  if (stopAnimations.has(id)) {
-      const stopFn = stopAnimations.get(id);
-      stopFn();
-      stopAnimations.delete(id);
-  }
-  const container = document.getElementById(id);
-  const layers = container.querySelectorAll(".overlay");
-  layers.forEach((layer) => {
-      layer.classList.remove("is-visible");
-  });
+function stopAnimation(id) {
+    if (stopAnimations.has(id)) {
+        const stopFn = stopAnimations.get(id);
+        stopFn();
+        stopAnimations.delete(id);
+    }
+    const container = document.getElementById(id);
+    const layers = container.querySelectorAll(".overlay");
+    layers.forEach((layer) => {
+        layer.classList.remove("is-visible");
+    });
 }
 
 function stopAllAnimations() {
-  // Stop all animations and hide their layers
-    containerIds.forEach((id) => {
-        stopAnimation(id);
+    // Stop all animations and hide their layers
+    document.querySelectorAll(".animation-container").forEach((container) => {
+        stopAnimation(container.id);
     });
 }
 
@@ -201,10 +182,7 @@ function toggleAnimation(containerId) {
     }
 }
 
-document
-    .querySelector('[data-map="oak-g"]')
-    .addEventListener("click", () => toggleAnimation("animated-redoak"));
-
-document
-    .querySelector('[data-map="maple-g"]')
-    .addEventListener("click", () => toggleAnimation("animated-maple"));
+function showOneAnimation(containerId) {
+    stopAllAnimations();
+    toggleAnimation(containerId);
+}
