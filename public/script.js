bbb0551b1a2a3b08edc89c441d00dd3d6efc5e76
Ali Berlin Johnson <allberlinjohnson@gmail.com>
2025-02-26 15:25:27 -0600

    do i need my own button?

--- public/script.js
+++ public/script.js
@@ -259,22 +259,3 @@ if ("serviceWorker" in navigator) {
 }
 
 
-let deferredPrompt;
-window.addEventListener("beforeinstallprompt", (event) => {
-  event.preventDefault();
-  deferredPrompt = event;
-
-  document.getElementById("install-btn").style.display = "block";
-  
-  document.getElementById("install-btn").addEventListener("click", () => {
-    deferredPrompt.prompt();
-    deferredPrompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome === "accepted") {
-        console.log("User accepted the install prompt");
-      }
-      deferredPrompt = null;
-    });
-  });
-});
-
-
