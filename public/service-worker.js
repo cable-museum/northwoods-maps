1c83654746c7aaf4424f694f51c2fe6c0b47691e
Ali Berlin Johnson <allberlinjohnson@gmail.com>
2025-03-13 16:04:55 -0500

    another try...

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,48 +1,36 @@
-const CACHE_NAME = "pwa-cache-v1";
-const ASSETS_TO_CACHE = [
-  "./", 
-  "./index.html",
-  "./manifest.json",
-  "./style.css",
-  "./script.js",
-  "./icon-192.png",
-  "./icon-512.png"
-];
+const CACHE_NAME = 'pwa-cache-v3';
 
-// Install Service Worker and cache assets
 self.addEventListener("install", (event) => {
-  console.log("Service Worker installing...");
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log("Caching assets...");
-      return cache.addAll(ASSETS_TO_CACHE);
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./style.css",
+        "./script.js",
+        "./icon-192.png",
+        "./icon-512.png"
+      ]);
     })
   );
 });
 
-// Activate Service Worker and remove old caches
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
+
+// DELETE OLD CACHES ON ACTIVATE
+//TODO: Geoffry is this ok?
 self.addEventListener("activate", (event) => {
-  console.log("Service Worker activated!");
   event.waitUntil(
     caches.keys().then((keys) => {
       return Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            console.log("Deleting old cache:", key);
-            return caches.delete(key);
-          }
-        })
+        keys
+          .filter((key) => key !== CACHE_NAME) // Keep only the latest version
+          .map((key) => caches.delete(key))
       );
-    })
-  );
-});
-
-// Intercept network requests and serve cached assets when offline
-self.addEventListener("fetch", (event) => {
-  console.log("Fetching:", event.request.url);
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      return cachedResponse || fetch(event.request);
     })
   );
 });
